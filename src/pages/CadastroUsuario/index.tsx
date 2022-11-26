884a40e1d2b52af53e836195e61f0f6903fae285
Daniela Leguari <danielaleguari@outlook.com>
2022-11-26 11:11:58 -0300

    ajustes mensagem de erro

--- src/pages/CadastroUsuario/index.tsx
+++ src/pages/CadastroUsuario/index.tsx
@@ -83,7 +83,7 @@ export const CadastroUsuario: React.FC = () => {
           </Grid>
           <Grid item xs={8} md={6}>
             <FormControl>
-              <FormLabel htmlFor="tipo-usuario">Selecione o tipo de usuário *</FormLabel>
+              <FormLabel htmlFor="tipo-usuario"> Tipo de usuário *</FormLabel>
               <Select error={Boolean(errors.tipoUsuario && errors.tipoUsuario.message)} id="tipo-usuario" value="a" labelId="label-tipo-usuario" size="small" {...register("tipoUsuario")} >
                 <MenuItem value="a" >Administrador</MenuItem>
                 <MenuItem value="i" >Instrutor(a)</MenuItem>
