9d0c7ca05955ec3956646fd47a80ab3d733e591d
Daniela Leguari <danielaleguari@outlook.com>
2022-11-27 10:42:12 -0300

    refatoracao cadastro de vagas

--- src/pages/CadastroVaga/index.tsx
+++ src/pages/CadastroVaga/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, FormControl, FormLabel, Grid, MenuItem, OutlinedInput, Select, Typography } from "@mui/material";
+import { Autocomplete, Box, Button, Container, FormControl, FormLabel, Grid, MenuItem, OutlinedInput, Select, TextField, Typography } from "@mui/material";
 import { HeaderPrincipal } from "../../components/HeaderPrincipal";
 import { IVagaForm } from "../../util/interface";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -15,100 +15,302 @@ export const CadastroVaga: React.FC = () => {
   });
 
   const { createVaga } = useContext(VagaContext);
+
+  const listaClientes = [
+    "Daniela", "Renan"
+  ];
+  const listaProgramas = [
+    "VemSer10", "VemSer11"
+  ];
+
   return (
-    <div>
-
+    <Grid
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '5%',
+      }}>
       <HeaderPrincipal />
-      <Container
+      <Box
         sx={{
-          mt: 4,
-          mb: 4,
-          width: '800px',
+          width: '80%',
+          height: '90%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '15px',
+          p: '15px 40px',
+          borderRadius: '15px',
           boxShadow: '-5px 7px 15px -4px rgba(0,0,0,0.75)',
-          height: '750px',
-          p: '15px',
-          borderRadius: '15px'
-        }} >
-        <Typography variant="h4" m={2} textAlign="center">Cadastro de vaga</Typography>
-        <form onSubmit={handleSubmit((data: IVagaForm) => {
+          margin: '30px'
+        }}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography fontSize='25px' color='primary'>Cadastro de Vaga</Typography>
+        </Box>
+        <Box component='form' id='form' onSubmit={handleSubmit((data: IVagaForm) => {
           createVaga(data);
-        })}>
-          <Grid container spacing={2}>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="cliente">Pesquisar Cliente*</FormLabel>
-                <OutlinedInput error={Boolean(errors.idCliente && errors.idCliente.message)}  id="nome-cliente" type="text" placeholder="Procurar cliente " size="small" {...register("idCliente")} />
-                {errors.idCliente && <Span className="error" texto={errors.idCliente.message} />}
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="vaga">Vaga*</FormLabel>
-                <OutlinedInput error={Boolean(errors.nome && errors.nome.message)}  id="nome-vaga" type="text" placeholder="Digite a vaga" size="small" {...register("nome")} />
-                {errors.nome && <Span className="error" texto={errors.nome.message} />}
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="codigo">Código*</FormLabel>
-                <OutlinedInput error={Boolean(errors.idVaga && errors.idVaga.message)}  id="codigo" type="text" placeholder="Digite o código" size="small" {...register("idVaga")} />
-                {errors.idVaga && <Span className="error" texto={errors.idVaga.message} />}
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="codigo">Programa*</FormLabel>
-                <OutlinedInput error={Boolean(errors.idPrograma && errors.idPrograma.message)} id="codigo" type="text" placeholder="Digite o programa" size="small" {...register("idPrograma")} />
-                {errors.idPrograma && <Span className="error" texto={errors.idPrograma.message} />}
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="situacao"> Situação*</FormLabel>
-                <Select error={Boolean(errors.situacao && errors.situacao.message)} id="situacao" labelId="situacao" size="small" defaultValue={"aberto"} {...register("situacao")}>
-                  <MenuItem value="aberto" >Aberto</MenuItem>
-                  <MenuItem value="fechado" >Fechado</MenuItem>
-                </Select>
-                {errors.situacao && <Span className="error" texto={errors.situacao.message} />}
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="vagas">Número de Vagas*</FormLabel>
-                <OutlinedInput error={Boolean(errors.quantidade && errors.quantidade.message)} id="vagas" type="number" size="small" {...register("quantidade")} />
-                {errors.quantidade && <Span className="error" texto={errors.quantidade.message} />}
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="data-abertura">Data de abertura*</FormLabel>
-                <OutlinedInput error={Boolean(errors.dataAbertura && errors.dataAbertura.message)} id="data-abertura" type="date" size="small" {...register("dataAbertura")} />
-                {errors.dataAbertura && <Span className="error" texto={errors.dataAbertura.message} />}
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl>
-                <FormLabel htmlFor="data-fechamento">Data de fechamento</FormLabel>
-                <OutlinedInput id="data-fechamento" type="date" size="small" {...register("dataFechamento")} />
-              </FormControl>
-            </Grid>
-            <Grid item xs={8} md={6}>
-              <FormControl fullWidth>
-                <FormLabel htmlFor="observacoes">Observações/Lembretes</FormLabel>
-                <OutlinedInput id="observacoes" type="text" placeholder="Observações" size="small" multiline minRows={3} {...register("observacoes")} />
-              </FormControl >
-            </Grid>
-          </Grid>
-          <input type="date" hidden id="data-criacao" {...register("dataCriacao")} />
-          <Box textAlign="center" mt={2}>
-            <Button type="submit" id="btn-salvar" variant="contained">Salvar</Button>
+        })}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '20px'
+          }}>
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '40px',
+          }}>
+            <TextField type="number" placeholder='Digite o código' id='idVaga' {...register("idVaga")} variant="outlined"
+              label='Código'
+              sx={{
+                width: '100%',
+                "& .MuiInputBase-input": {
+                  height: '10px'
+                }
+              }}
+              helperText={errors.idVaga && errors.idVaga.message ? errors.idVaga.message : null}
+              error={Boolean(errors.idVaga && errors.idVaga.message)}
+            />
+            <TextField type="text" placeholder='Nome da vaga' id='nome' {...register('nome')} variant="outlined"
+              label='Descrição'
+              sx={{
+                width: '100%',
+                "& .MuiInputBase-input": {
+                  height: '10px'
+                }
+              }}
+              helperText={errors.nome && errors.nome.message ? errors.nome.message : null}
+              error={Boolean(errors.nome && errors.nome.message)}
+            />
           </Box>
-        </form>
-      </Container>
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '40px',
+          }}>
+            {/*No options vai o array de clientes*/}
+            <Autocomplete options={listaClientes} id='idCliente' {...register("idCliente")}
+              renderInput={(params) => <TextField {...params} label='Selecionar Cliente' {...register("idCliente")}
+                helperText={errors.idCliente && errors.idCliente ? errors.idCliente.message : null}
+                error={Boolean(errors.idCliente && errors.idCliente.message)} />}
+              sx={{
+                width: '100%',
+                "& .MuiInputBase-input": {
+                  height: '10px'
+                }
+              }}
+            />
+            <Autocomplete options={listaProgramas} id='idPrograma' {...register("idPrograma")}
+              renderInput={(params) => <TextField {...params} label='Selecionar Programa' {...register("idPrograma")}
+                helperText={errors.idPrograma && errors.idPrograma ? errors.idPrograma.message : null}
+                error={Boolean(errors.idPrograma && errors.idPrograma.message)} />}
+              sx={{
+                width: '100%',
+                "& .MuiInputBase-input": {
+                  height: '10px'
+                }
+              }}
+            />
 
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'top',
+            gap: '40px'
+          }}>
+            <FormControl fullWidth error={Boolean(errors.quantidade && errors.quantidade.message)}>
+              <TextField type="number" id='quantidade'  {...register("quantidade")} variant="outlined"
+                label='Número de Vagas'
+                sx={{
+                  width: '100%',
+                  "& .MuiInputBase-input": {
+                    height: '10px'
+                  }
+                }}
+                helperText={errors.quantidade && errors.quantidade.message ? errors.quantidade.message : null}
+                error={Boolean(errors.quantidade && errors.quantidade.message)}
+              />
+            </FormControl>
+            <TextField type="number" placeholder='Quantidade de Alocados' id='quantidadeAlocados' {...register("quantidadeAlocados")} variant="outlined"
+              label='Quantidade de Alocados'
+              sx={{
+                width: '100%',
+                "& .MuiInputBase-input": {
+                  height: '10px'
+                }
+              }}
+        disabled
+            />
 
-    </div>
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'top',
+            gap: '40px'
+          }}>
+            <FormControl fullWidth error={Boolean(errors.dataAbertura && errors.dataAbertura.message)}>
+            <FormLabel htmlFor="data">Data de Abertura</FormLabel>
+              <TextField type="date" id='dataAbertura'  {...register("dataAbertura")} variant="outlined"
+                label=''
+                sx={{
+                  width: '100%',
+                  "& .MuiInputBase-input": {
+                    height: '10px'
+                  }
+                }}
+                helperText={errors.dataAbertura && errors.dataAbertura.message ? errors.dataAbertura.message : null}
+                error={Boolean(errors.dataAbertura && errors.dataAbertura.message)}
+              />
+            </FormControl>
+            <FormControl fullWidth >
+            <FormLabel htmlFor="data">Data de Fechamento</FormLabel>
+              <TextField type="date" id='dataFechamento'  {...register("dataFechamento")} variant="outlined"
+                label=''
+                sx={{
+                  width: '100%',
+                  "& .MuiInputBase-input": {
+                    height: '10px'
+                  }
+                }}
+              />
+            </FormControl>
+          </Box>
+
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'start',
+            gap: '40px',
+          }}>
+            <FormControl fullWidth error={Boolean(errors.situacao && errors.situacao.message)} >
+              <FormLabel htmlFor="situacao"> Situação</FormLabel>
+              <Select error={Boolean(errors.situacao && errors.situacao.message)} id="situacao" defaultValue={"aberto"} labelId="situacao" size="small" {...register("situacao")} >
+                <MenuItem value="aberto" >Aberto</MenuItem>
+                <MenuItem value="fechado" >Fechado</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <FormLabel htmlFor="observacoes">Observações/Lembretes</FormLabel>
+              <OutlinedInput id="observacoes" type="text" placeholder="Observações" size="small" multiline minRows={3} {...register("observacoes")} />
+            </FormControl >
+          </Box>
+          <Box sx={{ display: 'flex', gap: '40px', justifyContent: 'center', alignItems: 'center' }}>
+            <Box sx={{ width: '100%', textAlign: 'center' }}>
+              <input type="date" hidden id="data-criacao" {...register("dataCriacao")} />
+              <Button variant="contained" type="submit" sx={{
+                height: '50px'
+              }}>
+                Salvar
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Grid>
+
   );
+  // return (
+  //   <div>
+
+  //     <HeaderPrincipal />
+  //     <Container
+  //       sx={{
+  //         mt: 4,
+  //         mb: 4,
+  //         width: '800px',
+  //         boxShadow: '-5px 7px 15px -4px rgba(0,0,0,0.75)',
+  //         height: '750px',
+  //         p: '15px',
+  //         borderRadius: '15px'
+  //       }} >
+  //       <Typography variant="h4" m={2} textAlign="center">Cadastro de vaga</Typography>
+  //       <form onSubmit={handleSubmit((data: IVagaForm) => {
+  //         createVaga(data);
+  //       })}>
+  //         <Grid container spacing={2}>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="cliente">Pesquisar Cliente*</FormLabel>
+  //               <OutlinedInput error={Boolean(errors.idCliente && errors.idCliente.message)}  id="nome-cliente" type="text" placeholder="Procurar cliente " size="small" {...register("idCliente")} />
+  //               {errors.idCliente && <Span className="error" texto={errors.idCliente.message} />}
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="vaga">Vaga*</FormLabel>
+  //               <OutlinedInput error={Boolean(errors.nome && errors.nome.message)}  id="nome-vaga" type="text" placeholder="Digite a vaga" size="small" {...register("nome")} />
+  //               {errors.nome && <Span className="error" texto={errors.nome.message} />}
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="codigo">Código*</FormLabel>
+  //               <OutlinedInput error={Boolean(errors.idVaga && errors.idVaga.message)}  id="codigo" type="text" placeholder="Digite o código" size="small" {...register("idVaga")} />
+  //               {errors.idVaga && <Span className="error" texto={errors.idVaga.message} />}
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="codigo">Programa*</FormLabel>
+  //               <OutlinedInput error={Boolean(errors.idPrograma && errors.idPrograma.message)} id="codigo" type="text" placeholder="Digite o programa" size="small" {...register("idPrograma")} />
+  //               {errors.idPrograma && <Span className="error" texto={errors.idPrograma.message} />}
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="situacao"> Situação*</FormLabel>
+  //               <Select error={Boolean(errors.situacao && errors.situacao.message)} id="situacao" labelId="situacao" size="small" defaultValue={"aberto"} {...register("situacao")}>
+  //                 <MenuItem value="aberto" >Aberto</MenuItem>
+  //                 <MenuItem value="fechado" >Fechado</MenuItem>
+  //               </Select>
+  //               {errors.situacao && <Span className="error" texto={errors.situacao.message} />}
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="vagas">Número de Vagas*</FormLabel>
+  //               <OutlinedInput error={Boolean(errors.quantidade && errors.quantidade.message)} id="vagas" type="number" size="small" {...register("quantidade")} />
+  //               {errors.quantidade && <Span className="error" texto={errors.quantidade.message} />}
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="data-abertura">Data de abertura*</FormLabel>
+  //               <OutlinedInput error={Boolean(errors.dataAbertura && errors.dataAbertura.message)} id="data-abertura" type="date" size="small" {...register("dataAbertura")} />
+  //               {errors.dataAbertura && <Span className="error" texto={errors.dataAbertura.message} />}
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl>
+  //               <FormLabel htmlFor="data-fechamento">Data de fechamento</FormLabel>
+  //               <OutlinedInput id="data-fechamento" type="date" size="small" {...register("dataFechamento")} />
+  //             </FormControl>
+  //           </Grid>
+  //           <Grid item xs={8} md={6}>
+  //             <FormControl fullWidth>
+  //               <FormLabel htmlFor="observacoes">Observações/Lembretes</FormLabel>
+  //               <OutlinedInput id="observacoes" type="text" placeholder="Observações" size="small" multiline minRows={3} {...register("observacoes")} />
+  //             </FormControl >
+  //           </Grid>
+  //         </Grid>
+  //         <input type="date" hidden id="data-criacao" {...register("dataCriacao")} />
+  //         <Box textAlign="center" mt={2}>
+  //           <Button type="submit" id="btn-salvar" variant="contained">Salvar</Button>
+  //         </Box>
+  //       </form>
+  //     </Container>
+
+
+  //   </div>
+  // );
 }
 
 
