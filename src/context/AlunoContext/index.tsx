b9feb5694849c0113b03f4e8c06ff71bb538eaa5
Renan Cesar <cesar.renan@hotmail.com>
2022-11-28 16:54:15 -0300

    Adicionando função de post

--- src/context/AlunoContext/index.tsx
+++ src/context/AlunoContext/index.tsx
@@ -1,15 +1,35 @@
+import nProgress from "nprogress";
 import { createContext, useState } from "react";
+import { API } from "../../util/api";
+import { toastConfig } from "../../util/toast";
 import { TAlunoContext, TChildren, TAluno } from "../../util/types";
+import { toast } from "react-toastify";
+import { useNavigate } from 'react-router-dom'
 
 export const AlunoContext = createContext({} as TAlunoContext);
 
 export const AlunoProvider = ({ children }: TChildren) =>{
-
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
     const [ radioValue, setRadioValue] = useState<any>('');
     
-    const handleCreateAluno = async (aluno : TAluno)=>{        
+    const handleCreateAluno = async (aluno : TAluno)=>{
         aluno.tipoVaga = radioValue;
-        console.log(aluno);
+        aluno.tipoVaga = aluno.tipoVaga.toUpperCase();
+        
+        try{
+            nProgress.start();
+            // API.defaults.headers.common["Authorization"] = token;
+            // await API.post('/aluno', aluno)
+            toast.success("Aluno cadastrado com sucesso!", toastConfig);
+            console.log(aluno);
+            navigate('/alunos')
+        } catch (error){
+            toast.error('Houve algum erro, tente novamente mais tarde.', toastConfig)
+        } finally {
+            nProgress.done();
+        }
+
     }
 
     return(
