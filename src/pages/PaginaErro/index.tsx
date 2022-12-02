fd6cdf3586a424e99feabc1c55e8c2c9454c284e
Renan Cesar <cesar.renan@hotmail.com>
2022-12-02 16:10:37 -0300

    atualizando página de erros

--- src/pages/PaginaErro/index.tsx
+++ src/pages/PaginaErro/index.tsx
@@ -1,6 +1,6 @@
 import ReportIcon from '@mui/icons-material/Report';
 import { Link } from 'react-router-dom';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Button, Box } from '@mui/material';
 
 
 export const PaginaErro = () => {
@@ -17,7 +17,37 @@ export const PaginaErro = () => {
       }}>
         <ReportIcon color='error' sx={{fontSize:"7rem"}}/>
         <Typography variant="h4" color='primary'>A página que você procura não foi encontrada!</Typography>
-        <Link style={{ textDecoration: 'none'}} to='/painel-vagas'><Typography variant="h6" color='secundary' sx={{fontWeight:'bold'}}>Voltar para página inicial</Typography></Link>
+        <Box
+                    sx={{
+                      marginTop: "20px",
+                      width: "100%",
+                      display: "flex",
+                      justifyContent: "center",
+                      transition: ".8s",
+                      height: '80px',
+
+                      "& :hover": {
+                        transform: "scale(1.02)",
+                        transition: ".8s",
+                      },
+
+                      "& :active": {
+                        transform: "scale(.99)",
+                      },
+                    }}
+                  >
+                    <Link style={{ textDecoration: 'none'}} to='/painel-vagas'>
+                    <Button                      
+                      size="large"
+                      variant="contained"
+                      sx={{
+                        fontSize: "10px",
+                      }}
+                    >
+                      Voltar para o início
+                    </Button></Link>
+                  </Box>
+        {/* <Typography variant="h6" color='secundary' sx={{fontWeight:'bold'}}>Voltar para página inicial</Typography> */}
     
     </Grid>
   );
